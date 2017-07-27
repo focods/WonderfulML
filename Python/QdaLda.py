4f9fafdc126585cef9268251a0438d522a181713
Michael Szczepaniak <golf4enlightenment@yahoo.com>
2017-07-27 11:52:55 -0600

    update doc string for normald function in python

--- Python/QdaLda.py
+++ Python/QdaLda.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 
 def normald(X, mu=None, sigma=None):
     """ Returns the normal probability density for a D-dimensional vector X
-        X 
+        X - n (samples) x d (dimensions) matrix (2D numpy array) of samples
+        mu - 1 x d matrix of the d-dimensional mean of the distribution
+        sigma - d x d covariance matrix of the d-dimensional distribution
+        
+        If no samples (xs) are supplies, just a definition is returned.
+        If no means (mu) are supplied, assume std. norm. dist. w/ all mu=0.
+        If no sigma covariance matrix is supplied, assume std. normal dist.
+         w/ sigma = the d x d identity matrix
     """
     
     

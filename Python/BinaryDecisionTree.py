fb3039ff3b019b3f8ef841284eb1aec3554a35a0
Michael Szczepaniak <mszczepaniak@pobox.com>
2017-08-10 20:42:21 -0600

    remove TODO on gini_index2 function: need class_values

--- Python/BinaryDecisionTree.py
+++ Python/BinaryDecisionTree.py
@@ -20,7 +20,6 @@ def gini_index(groups, class_values):
 # Calculate the Gini index for a split dataset accounting for group size.
 # groups - tuple or list of samples corresponding to a split group
 # class_values - tuple or list of unique class labels
-# TODO class_values can be obtained from groups - no need to pass in
 def gini_index2(groups, class_values):
     sample_count = sum([len(group) for group in groups])
     gini_groups = []

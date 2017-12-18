005593cdc35c4c46e8dcdf2b4ad4b50e6bc3bb9c
Michael Szczepaniak <mszczepaniak@pobox.com>
2017-12-17 21:28:25 -0700

    impl appendAllCol function which groups dynamically

--- R/DataTools.R
+++ R/DataTools.R
@@ -25,3 +25,48 @@ trainSetRows <- function(orig, fraction, seed=711) {
 testSetRows <- function(allRows, trainRows) {
     return(setdiff(allRows, trainRows))
 }
+
+## Groups df by df_group_by_col_name, sums over it and adds
+## new rows with f_name = 'all' and df_group_by_col_name as the sum
+## over the group.  TODO make the f_name a variable
+appendAllCol <- function(df, df_group_by_col_name, all_name = 'all') {
+    df_group_by_col_name <- enquo(df_group_by_col_name)
+    
+    ret_df <- df %>% group_by(date) %>%
+              summarize(!!quo_name(df_group_by_col_name) :=
+                            sum(!!df_group_by_col_name)) %>% 
+              mutate(f_name = all_name)
+    # ret_df <- ret_df[c(1,3,2)]  # no need to realign col's
+    
+    ret_df <- bind_rows(df, ret_df)
+    
+    return(ret_df)
+}
+
+## test data
+d <- seq(as.Date('2017/01/01'), as.Date('2017/01/08'), "days")
+first_name <- rep("Jane", 8)
+first_name <- append(first_name, rep("Fred", 8))
+first_name <- append(first_name, rep("Sally", 8))
+dat <- data.frame(date=d, f_name=first_name, x=seq(1, 3*8, 1),
+                  stringsAsFactors = FALSE)
+
+# d2 <- group_by_(dat, 'date') %>% summarize_(daily_total = sum_('x'))
+d3 <- mutate(df1, a = 3)
+
+mutate_y <- function(df) {
+    mutate(df, y = .data$a + .data$x)
+}
+
+df <- tibble(
+    g1 = c(1, 1, 2, 2, 2),
+    g2 = c(1, 2, 1, 2, 1),
+    a = sample(5), 
+    b = sample(5)
+)
+
+
+
+
+
+

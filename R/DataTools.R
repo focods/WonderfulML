bfd7dfca70b5b9163573a00044d8b4c022713d4e
Michael Szczepaniak <golf4enlightenment@yahoo.com>
2017-12-18 20:03:39 -0700

    finish impl'ing addAllRows function using referential transp'y

--- R/DataTools.R
+++ R/DataTools.R
@@ -26,44 +26,37 @@ testSetRows <- function(allRows, trainRows) {
     return(setdiff(allRows, trainRows))
 }
 
-## Groups df by df_group_by_col_name, sums over it and adds
-## new rows with f_name = 'all' and df_group_by_col_name as the sum
+## Groups df by df_sum_over_col, sums over it and adds
+## new rows with f_name = 'all' and df_sum_over_col as the sum
 ## over the group.  TODO make the f_name a variable
-appendAllCol <- function(df, df_group_by_col_name, all_name = 'all') {
-    df_group_by_col_name <- enquo(df_group_by_col_name)
-    
-    ret_df <- df %>% group_by(date) %>%
-              summarize(!!quo_name(df_group_by_col_name) :=
-                            sum(!!df_group_by_col_name)) %>% 
-              mutate(f_name = all_name)
-    # ret_df <- ret_df[c(1,3,2)]  # no need to realign col's
-    
+addAllRows <- function(df, df_groupby_col, df_sum_over_col,
+                       df_all_label_col, all_label = 'all') {
+    quo_df_groupby_col <- enquo(df_groupby_col)
+    df_sum_over_col <- enquo(df_sum_over_col)
+    df_all_label_col <- quo_name(enquo(df_all_label_col))
+    # build the records for the sums of df_groupby_col item
+    ret_df <- df %>% group_by(!!quo_df_groupby_col) %>%
+              summarize(!!quo_name(df_sum_over_col) := sum(!!df_sum_over_col)) %>% 
+              mutate(!!df_all_label_col := all_label)
+    # we now have all our, now just append them to the df and return
     ret_df <- bind_rows(df, ret_df)
     
     return(ret_df)
 }
 
 ## test data
-d <- seq(as.Date('2017/01/01'), as.Date('2017/01/08'), "days")
-first_name <- rep("Jane", 8)
-first_name <- append(first_name, rep("Fred", 8))
-first_name <- append(first_name, rep("Sally", 8))
-dat <- data.frame(date=d, f_name=first_name, x=seq(1, 3*8, 1),
-                  stringsAsFactors = FALSE)
-
-# d2 <- group_by_(dat, 'date') %>% summarize_(daily_total = sum_('x'))
-d3 <- mutate(df1, a = 3)
-
-mutate_y <- function(df) {
-    mutate(df, y = .data$a + .data$x)
+getDTTestData <- function() {
+    d <- seq(as.Date('2017/01/01'), as.Date('2017/01/08'), "days")
+    first_name <- rep("Jane", 8)
+    first_name <- append(first_name, rep("Fred", 8))
+    first_name <- append(first_name, rep("Sally", 8))
+    dat <- data.frame(date=d, f_name=first_name, x=seq(1, 3*8, 1),
+                      stringsAsFactors = FALSE)
+    return(dat)
 }
 
-df <- tibble(
-    g1 = c(1, 1, 2, 2, 2),
-    g2 = c(1, 2, 1, 2, 1),
-    a = sample(5), 
-    b = sample(5)
-)
+
+
 
 
 
